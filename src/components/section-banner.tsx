8f3d790db0dbb4ba9416644109c5affa13558601
Elton Santos <elton.santos@uhgbrasil.com.br>
2024-02-25 00:57:42 -0300

    changed the color of the div and put two divs in the banner

--- src/components/section-banner.tsx
+++ src/components/section-banner.tsx
@@ -8,9 +8,33 @@ interface BannerProps {
 
 const BannerSection: React.FC<BannerProps> = ({ imageUrl, altText }) => {
   return (
-    <section className="section-banner">
-      <img src={imageUrl} alt={altText} />
-      <h1>Banner</h1>
+    <section className="section-banner">      
+      <div className="div_banner_one">
+        <span><b>What is Lorem Ipsum?</b></span>      
+        <p>
+          <b>Lorem Ipsum</b> is simply dummy text of the printing and typesetting industry. 
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+          when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+          It has survived not only five centuries, but also the leap into electronic typesetting, 
+          remaining essentially unchanged. It was popularised in the 1960s with the release of 
+          Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
+          software like Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
+      </div>
+
+      <div className="div_banner_two">
+        <span><b>Where does it come from?</b></span>
+        <p>
+            <b>Lorem Ipsum</b> is simply dummy text of the printing and typesetting industry. Lorem 
+            Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown 
+            printer took a galley of type and scrambled it to make a type specimen book. It has survived 
+            not only five centuries, but also the leap into electronic typesetting, remaining essentially 
+            unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem 
+            Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including 
+            versions of Lorem Ipsum.
+          </p>
+      </div>
+      
     </section>
     
   );
